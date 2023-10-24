c0226e02001b236180700266591de4fecdd4d66c
Herjus <herjus420@gmail.com>
2023-10-24 17:51:15 +0200

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	const servers = [{}, {}, {}]
+</script>
+
+<main class="flex h-screen justify-between">
+	<div class="w-16 h-full p-2 space-y-2 overflow-y-scroll hide-scrollbar">
+		<div class="hover:cursor-pointer bg-primary-400 hover:rounded-xl rounded-2xl duration-200 w-full aspect-square"></div>
+		<div class="h-1 bg-surface-700"></div>
+		{#each servers as server}
+			<div class="hover:cursor-pointer bg-primary-400 hover:rounded-xl rounded-2xl duration-200 w-full aspect-square"></div>
+		{/each}
+	</div>
+	<div class="flex-1 h-full justify-between flex flex-row">
+		<div class="bg-surface-500 w-72 h-full"></div>
+		<div class="flex-1 bg-surface-600 h-full"></div>
+		<div class="w-72 h-full"></div>
+	</div>
+</main>
