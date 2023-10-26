a1be67d7256d06528778827bc3ba1072dd7d6451
Herjus <herjus420@gmail.com>
2023-10-26 19:12:02 +0200

    updates to the signin func and added support for db

--- backend/src/auth.rs
+++ backend/src/auth.rs
@@ -1,3 +1,5 @@
+mod jwt;
+
 use axum::{
     http::StatusCode,
     Json,
@@ -6,18 +8,12 @@ use pwhash::bcrypt;
 use serde::{Deserialize, Serialize};
 
 pub async fn register_user(Json(payload): Json<CreateUser>) -> (StatusCode, Json<User>) {
-    // let hash = bcrypt
-
-    let pass = payload.password.clone();
-
     let user = User {
         id: 1234,
         username: payload.username,
         email: payload.email,
         hashed_password: bcrypt::hash(payload.password).unwrap(),
     };
-
-    // assert_eq!(bcrypt::verify(pass, user.hashed_password.as_str()), true);
 
     (StatusCode::CREATED, Json(user))
 }
@@ -37,13 +33,36 @@ pub struct User {
     hashed_password: String,
 }
 
+// pub async fn sign_in(Json(payload): Json<SignInUser>) -> Json<SignInReturn>{
+//     let ret = SignInReturn {
+//         token: "lol".to_string(),
+//         message: "ok".to_string(),
+//     };
+// }
+
+#[derive(Deserialize)]
+pub struct SignInUser {
+    email: String,
+    password: String,
+}
+
+#[derive(Serialize)]
+pub struct SignInReturn {
+    token: String,
+    message: String,
+}
+
 pub async fn validate(Json(payload): Json<ValidateUser>) -> (StatusCode, Json<ValidatedUser>) {
 
     let res = ValidatedUser {
         validated: bcrypt::verify(payload.password, payload.hashed_password.as_str())
     };
 
-    (StatusCode::OK, Json(res))
+    if res.validated {
+        (StatusCode::OK, Json(res))
+    } else {
+        (StatusCode::BAD_REQUEST, Json(res))
+    }
 }
 
 #[derive(Deserialize)]
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,12 +1,24 @@
 mod auth;
 
+use std::string::ToString;
 use axum::{
     routing::get, routing::post,
     Router,
 };
+use dotenv::dotenv;
+
+static CLIENT: Option<None> = libsql_client::Client::from_config(libsql_client::Config {
+    url: url::Url::parse(std::env::var("DATABASE_URL").expect("DATABASE_URL must be set.").as_ref()).unwrap(),
+    auth_token: Option::from(std::env::var("DATABASE_TOKEN").expect("DATABASE_URL must be set.")),
+});
+
 
 #[tokio::main]
 async fn main(){
+    dotenv().ok();
+
+    CLIENT.lock().unwrap();
+
     let app = Router::new()
         .route("/register", post(auth::register_user))
         .route("/validate", post(auth::validate));
