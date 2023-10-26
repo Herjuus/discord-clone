f2599054a667c73db5a6c9a5ba40902c6f5ccb45
Herjus <herjus420@gmail.com>
2023-10-26 19:19:18 +0200

    More errors

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,16 +1,23 @@
 mod auth;
 
 use std::string::ToString;
+use std::sync::Mutex;
 use axum::{
     routing::get, routing::post,
     Router,
 };
 use dotenv::dotenv;
+use once_cell::sync::Lazy;
 
-static CLIENT: Option<None> = libsql_client::Client::from_config(libsql_client::Config {
-    url: url::Url::parse(std::env::var("DATABASE_URL").expect("DATABASE_URL must be set.").as_ref()).unwrap(),
-    auth_token: Option::from(std::env::var("DATABASE_TOKEN").expect("DATABASE_URL must be set.")),
-});
+static CLIENT: Lazy<Mutex<libsql_client>> = Lazy::new(|| {
+    let db = libsql_client::Client::from_config(libsql_client::Config {
+        url: url::Url::parse(std::env::var("DATABASE_URL").expect("DATABASE_URL must be set.").as_ref()).unwrap(),
+        auth_token: Option::from(std::env::var("DATABASE_TOKEN").expect("DATABASE_URL must be set.")),
+    });
+
+    Mutex::new(db);
+})
+
 
 
 #[tokio::main]
