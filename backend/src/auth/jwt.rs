817976e3e971c3411d0f142bd063b5c7e6525bad
Herjus <herjus420@gmail.com>
2023-11-02 15:32:40 +0100

    generate-user-token function

--- backend/src/auth/jwt.rs
+++ backend/src/auth/jwt.rs
@@ -1,3 +1,25 @@
-pub fn generate_user_token(uid: i32) -> String {
-    
+use axum::http::StatusCode;
+use chrono::{Duration, Utc};
+use jsonwebtoken::{encode, Algorithm, Header, EncodingKey};
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+struct Claims {
+    exp: usize,
+    user_id: i32,
+}
+
+pub fn generate_user_token(uid: i32) -> Result<String, StatusCode> {
+    let mut time = Utc::now();
+    let expires_in = Duration::minutes(1);
+    time += expires_in;
+
+    let claims = Claims {
+        exp: time.timestamp() as usize,
+        user_id: uid,
+    };
+
+    let key = EncodingKey::from_secret(std::env::var("JWT_SECRET").unwrap().as_ref());
+
+    encode(&Header::default(), &claims, &key).map_err(|err| StatusCode::INTERNAL_SERVER_ERROR)
 }
