3c6079838dc0339a359a8d09c80e2f2e8d5d2b2a
Herjus <herjus420@gmail.com>
2023-11-08 12:41:49 +0100

    Cli test

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -3,56 +3,9 @@
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
 	import { storePopup } from '@skeletonlabs/skeleton';
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
-
-	import { invoke } from '@tauri-apps/api';
-
-	setTimeout(() => {
-		invoke('close_splashscreen');
-	}, 3000);
-
-	let loggedIn: boolean = false;
-	let loading: boolean = false;
-
-	function handleSignin(e: Event) {
-		e.preventDefault();
-		loading = true;
-		setTimeout(() => {
-			loading = false;
-			// loggedIn = true;
-		}, 1000);
-	}
+	
 </script>
 
 <main>
-	{#if loggedIn}
-		<slot />
-	{:else}
-		<div class="w-screen h-screen backdrop-blur-sm flex flex-col justify-center items-center">
-			<form on:submit={handleSignin} class="card rounded p-4 w-96 space-y-5 flex flex-col">
-				<div>
-					<h3 class="h3">Login</h3>
-					<span class="text-tertiary-600 text-sm">Sign into your account to continue</span>
-				</div>
-				<div class="flex flex-col">
-					<input autocomplete="off" class="order-2 input focus:border-primary-500 invalid:border-error-500 invalid:focus:border-error-500 rounded text-sm peer" placeholder="name@example.com" type="email">
-					<span class="order-1 text-tertiary-800 text-sm peer-focus:text-tertiary-600 duration-200">Email adress</span>
-					<span class="order-2 hidden peer-invalid:block text-error-500 text-sm">
-						Invalid email address
-					</span>
-				</div>
-				<div class="flex flex-col">
-					<input autocomplete="off" class="order-2 input focus:border-primary-500 invalid:border-error-500 invalid:focus:border-error-500 rounded text-sm peer" placeholder="********" type="password">
-					<span class="order-1 text-tertiary-800 text-sm peer-focus:text-tertiary-600 duration-200">Password</span>
-					<span class="order-2 hidden peer-invalid:block text-error-500 text-sm">
-						Invalid password
-					</span>
-				</div>
-				<button disabled={loading} type="submit" class="btn rounded variant-filled-primary text-tertiary-500 text-sm w-min">{#if loading}
-					Signing in...
-					{:else}
-					Sign in
-				{/if}</button>
-			</form>
-		</div>
-	{/if}
+	<slot />
 </main>
--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,28 +1,18 @@
-<script>
-	const servers = [{}, {}, {}]
-	import { Plus } from 'lucide-svelte'
+<script lang="ts">
+	import { invoke } from '@tauri-apps/api';
+    import SignIn from '../components/sign-in.svelte';
+    import Main from '../components/main.svelte';
+
+	setTimeout(() => {
+		invoke('close_splashscreen');
+	}, 3000);
+
+	let loggedIn: boolean = false;
+
 </script>
 
-<main class="flex h-screen justify-between">
-	<div class="w-[4rem] h-full p-2 space-y-2">
-		<!-- <div class="flex justify-between">
-			<div class="w-3 aspect-square bg-red-500 hover:bg-red-600 rounded-full hover:cursor-pointer"></div>
-			<div class="w-3 aspect-square bg-yellow-500 hover:bg-yellow-600 rounded-full hover:cursor-pointer"></div>
-			<div class="w-3 aspect-square bg-green-500 hover:bg-green-600 rounded-full hover:cursor-pointer"></div>
-		</div> -->
-		<div class="h-full space-y-2 overflow-y-scroll hide-scrollbar">
-			<div class="hover:cursor-pointer bg-primary-400 hover:rounded-xl rounded-2xl duration-200 w-full aspect-square"></div>
-			<div class="h-1 bg-surface-700"></div>
-			{#each servers as server}
-				<div class="hover:cursor-pointer bg-primary-400 hover:rounded-xl rounded-2xl duration-200 w-full aspect-square"></div>
-			{/each}
-			<div class="h-1 bg-surface-700"></div>
-			<div class="hover:cursor-pointer bg-secondary-400 hover:rounded-xl rounded-2xl duration-200 w-full aspect-square flex justify-center items-center"><Plus size=32 /></div>
-		</div>
-	</div>
-	<div class="flex-1 h-full justify-between flex flex-row">
-		<div class="bg-surface-500 w-64 h-full"><button></button></div>
-		<div class="flex-1 bg-surface-600 h-full"></div>
-		<div class="w-72 h-full"></div>
-	</div>
-</main>
+{#if loggedIn}
+		<Main />
+	{:else}
+		<SignIn />
+{/if}
